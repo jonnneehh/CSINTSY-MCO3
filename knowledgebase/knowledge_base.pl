c7dc28561d43115e85d12f9e3b34b316629f020a
Jon Llamado <jon_llamado@dlsu.edu.ph>
2022-02-11 01:03:21 +0800

    finished knowledge base

--- knowledgebase/knowledge_base.pl
+++ knowledgebase/knowledge_base.pl
@@ -1,5 +1,20 @@
+/*
+['c:/programs/java/csintsy-mco3/knowledgebase/knowledge_base.pl'].
+*/
 
 /* PROFILE INFORMATION */
+mustBeFullyVaccinated(IsCanadian, IsForeigner, IsWorker) :-
+    IsCanadian; IsForeigner; IsWorker.
+
+mustBeFullyVaccinated(IsWorker, IsJobExempted) :-
+    (   IsWorker, IsJobExempted
+    ->  false
+    ;   true
+    ).
+
+underFive(Age) :- Age < 5.
+minor(Age) :- Age >= 5, Age =< 18.
+
 isInList([H|T], H).
 isInList([_|T], H) :- isInList(T, H).
 
@@ -14,17 +29,7 @@ isLastDose14DaysBefore(DaysSinceLastDose):-
     DaysSinceLastDose >= 14.
 
 isNoSymptoms(true):- true.
-isNoSymptoms(false) :- false.
 
-mustBeFullyVaccinated(true) :- true.
-
-/* IF traveller is a Canadian citizen THEN traveller must be fully vaccinated to travel to Canada */
-canadian(true):- mustBeFullyVaccinated(true).
-
-/* IF traveller is a foreign nationale THEN traveller must be fully vaccinated to travel to Canada */
-foreigner(true) :-  mustBeFullyVaccinated(true).
-
-/* IF received full dose of any valid vaccines and last dose was 14 days before arrival in Canada and show no symptoms of COVID-19 THEN traveler is fully vaccinated */
 fullyVaccinated(Doses, Vaccine, DaysSinceLastDone, NoSymptoms) :-
     (   isFullDoseOfValidVaccines(Doses, Vaccine),
         isLastDose14DaysBefore(DaysSinceLastDone),
@@ -32,67 +37,23 @@ fullyVaccinated(Doses, Vaccine, DaysSinceLastDone, NoSymptoms) :-
     ->  true
     ).
 
+
 /* TRAVEL REQUIREMENTS */
-increment(NewI, I) :-
-    NewI is I + 1.
-
-infoCheckRec(I, Eligibility) :-
-    infoCheck(I, Eligibility),
-    increment(NewI, I), write(I), write(" "),
-    (   I =< 7 -> infoCheckRec(NewI, Eligibility) ).
-
-infoCheck(I, Eligibility):-
-    (   I = 0
-    ->  mustSubmitInfoToArriveCan(Eligibility)
-    ;   I = 1
-    ->  mustProvideProofVaccination(Eligibility)
-    ;   I = 2
-    ->  mustProvideNegativeMolecularTest(Eligibility)
-    ;   I = 3
-    ->  mustProvideArriveCanReceipt(Eligibility)
-    ;   I = 4
-    ->  mustHaveQuarantinePlan(Eligibility)
-    ;   I = 5
-    ->  mustBringTravelDocument(Eligibility)
-    ;   I = 6
-    ->  mustRegisterArrivalTesting(Eligibility)
-    ;   I = 7
-    ->  possibleArrivalTestingQuarantine(Eligibility)
-    ).
-
-/* IF traveller qualifies as fully vaccinated THEN traveller must submit profile information and travel details to ArriveCan */
 mustSubmitInfoToArriveCan(true) :- true.
-mustSubmitInfoToArriveCan(false) :- false.
-
-/* IF traveller qualifies as fully vaccinated THEN traveller must provide proof of vaccination that was also submitted to ArriveCan */
 mustProvideProofVaccination(true) :- true.
-mustProvideProofVaccination(false) :- false.
-
-/* IF traveller qualifies as fully vaccinated THEN traveller must provide a valid negative molecular test result */
 mustProvideNegativeMolecularTest(true) :- true.
-mustProvideNegativeMolecularTest(false) :- false.
-
-/* IF traveller qualifies as fully vaccinated THEN traveller must provide an ArriveCan receipt with letter I, V, or A, beside the traveller’s name */
 mustProvideArriveCanReceipt(true) :- true.
-mustProvideArriveCanReceipt(false) :- false.
-
-/* IF traveller qualifies as fully vaccinated THEN traveller must have a quarantine plan */
 mustHaveQuarantinePlan(true) :- true.
-mustHaveQuarantinePlan(false) :- false.
-
-
-/* IF traveller qualifies as fully vaccinated THEN traveller must bring the travel document that was submitted to ArriveCan */
 mustBringTravelDocument(true) :- true.
-mustBringTravelDocument(false) :- false.
-
-/* IF traveller qualifies as fully vaccinated THEN traveller must register in advance for arrival testing */
 mustRegisterArrivalTesting(true) :- true.
-mustRegisterArrivalTesting(false) :- false.
+
+mustBeAccompaniedByFullyVaxedAdult(Age) :- minor(Age).
 
 /* TRAVEL RULES */
-
-/* IF traveller qualifies as fully vaccinated THEN then traveller may be randomly selected for a mandatory arrival test and must quarantine while waiting for results */
 possibleArrivalTestingQuarantine(true) :- true.
-possibleArrivalTestingQuarantine(false) :- false.
 
+mustFollowRulesForMinors(Age) :- minor(Age).
 
+exemptPreEntryTest(Age) :- underFive(Age).
+exemptArrivalTest(Age) :- underFive(Age).
+exemptDay8Test(Age) :- underFive(Age).
